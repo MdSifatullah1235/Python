ad8c865a6b42e73f38f72ee8489276ce108b6b9c
MdSifatullah1235 <miasifat1235@gmail.com>
2026-01-15 18:53:48 +0600

    v1

--- 1,2,3,More shot learning/Project.py
+++ 1,2,3,More shot learning/Project.py
@@ -1,18 +1,16 @@
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
+import config
 
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-model = GPT2LMHeadModel.from_pretrained('gpt2')
-
+model=GPT2LMHeadModel.from_pretrained('gpt2')
 
 def get_response(prompt,max_length=50):
-    inputs = tokenizer(prompt, return_tensors='pt')
-    output = model.generate(**inputs, max_length=max_length,num_return_sequences=1)
-    response = tokenizer.decode(output[0], skip_special_tokens=True)
+    inputs=tokenizer.encode(prompt,return_tensors='pt')
+    outputs=model.generate(inputs,max_length=max_length,num_return_sequences=1)
+    response=tokenizer.decode(outputs[0],skip_special_tokens=True)
     return response
 
-question_prompt = input("Enter a question: ")
-print(f"Question Prompt: {get_response(question_prompt)}")
-
-command_prompt = input("Enter a command: ")
-
-print(f"Command Prompt: {get_response(command_prompt)}")
+question_prompt=input("Ask me a question: ")
+print(f"Question prompt response:{get_response(question_prompt)}")
+command_prompt=input("Tell me something to do: ")
+print(f"Command prompt response:{get_response(command_prompt)}")
