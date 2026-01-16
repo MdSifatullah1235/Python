9030fe20e1204cf2b5f3bb4c92cb444d4855dd38
MdSifatullah1235 <miasifat1235@gmail.com>
2026-01-16 09:59:04 +0600

    v1

--- ACP's/GeminiChatbot.py
+++ ACP's/GeminiChatbot.py
@@ -1,11 +1,10 @@
 from google import genai
-from google.genai import types
 import config
-from colorama import init, Style,Fore
+from colorama import init, Style, Fore
 
 init(autoreset=True)
-client = genai.Client(api_key=config.GEMINI_API_KEY)
 
+client = genai.Client(api_key=config.GEMINI_API_KEY)
 
 def run_chatbot():
     chat = client.chats.create(model="gemini-2.0-flash")
@@ -15,16 +14,16 @@ def run_chatbot():
     while True:
         try:
             user_msg = input(f'{Fore.YELLOW} You: {Style.RESET_ALL}')
-            if user_msg.lower() in ["bye","exit","quit","end","stop"]:
+            
+            if user_msg.lower() in ["bye", "exit", "quit", "end", "stop"]:
                 print(f"{Fore.GREEN} Have a nice day! {Style.RESET_ALL}")
                 break
 
             response = chat.send_message(user_msg)
-            print(f"{Fore.GREEN} Chatbot: {Style.RESET_ALL}{response.text}")
+            print(f"{Fore.GREEN} Chatbot: {Style.RESET_ALL}{response.text}", flush=True)
         
         except Exception as e:
             print(f"{Fore.RED}Error: {e}")
-
 
 if __name__ == "__main__":    
     run_chatbot()
