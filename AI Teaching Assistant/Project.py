ccce6dfb5d320b3ed2841779c37b84023cb215dc
MdSifatullah1235 <miasifat1235@gmail.com>
2026-01-26 22:12:41 +0600

    v1

--- AI Teaching Assistant/Project.py
+++ AI Teaching Assistant/Project.py
@@ -1,42 +1,30 @@
 import streamlit as st
-
 from google import genai
 from google.genai import types
-from config import GEMINI_API_KEY
-
-client = genai.Client(api_key=GEMINI_API_KEY)
-
-
-
+from config import GEMINI_apikey
+client = genai.Client(api_key=GEMINI_apikey)
 def generate_response(prompt,temperature=0.3):
     try:
-        contents=[types.Content(role="user",parts=[types.Part.from_text(text=prompt)])]
+        contents=[types.Content(role="user",parts={types.Part.from_text(text=prompt)})]
         config_params=types.GenerateContentConfig(temperature=temperature)
         response=client.models.generate_content(model="gemini-2.0-flash",contents=contents,config=config_params)
         return response.text
     except Exception as e:
-        return f"Error {str(e)}"
-
-
+        return f"Error{str(e)}"
+    
 def setup_ui():
-    st.title("AI Teaching Assistant")
-    st.write("You can ask me about any subject, topic or anything in general")
-
-    user_input = st.text_input("Enter your question: ")
-
+    st.title("Ai teaching assistant")
+    st.write("Welcome you can ask me anything about various subjects and i will provide an answer")
+    user_input=st.text_input("Enter your question here: ")
     if user_input:
-        st.write("Your question: ", user_input)
-
-        response = generate_response(user_input)
-        st.write("AI's response: ", response)
-
+        st.write(f"Your questions: {user_input}")
+        response=generate_response(user_input)
+        st.write(f"Answer: {response}")
     else:
-        st.write("Please enter a question.")
-
+        st.write("Please enter a question to ask")
 
 def main():
     setup_ui()
 
-
-if __name__ == "__main__":
+if __name__=="__main__":
     main()
--- AI Teaching Assistant/config.py
+++ AI Teaching Assistant/config.py
@@ -1,1 +1,1 @@
-GEMINI_API_KEY = "AIzaSyDbXzhPPacvr53JfF-DcLCXQlu2qbxq1VM" 
+GEMINI_API_KEY = "" 
